c8345dacd8e5995f242ce5226a02e286a4acae21
Natã Junior <natajuniorpinheirodasilva@gmail.com>
2026-07-24 21:20:04 -0300

    add handleSubmit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,42 @@
 'use client'
 import { useState } from "react"
 
+type Task = {
+  id: string;
+  title: string;
+  description: string;
+  status: string;
+  priority: string;
+}
+
 export default function Home() {
   const [title, setTitle] = useState<string>("")
   const [description, setDescription] = useState<string>("")
   const [status, setStatus] = useState<string>("")
   const [priority, setPriority] = useState<string>("")
-
   const [newTask, setNewTask] = useState<boolean>(false)
+  
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  const handleSubmit = (e: React.SubmitEvent<) => {
+    e.preventDefault()
+
+    const newTask: Task = {
+      id: crypto.randomUUID(),
+      title,
+      description,
+      status,
+      priority
+    }
+
+    setTasks([...tasks, newTask])
+
+    setTitle('')
+    setDescription('')
+    setStatus('')
+    setPriority('')
+    setNewTask(false)
+  }
   
   return (
     <div className="p-4">
@@ -19,33 +48,36 @@ export default function Home() {
         </h1>
 
         <button
-        className="bg-emerald-800 text-white p-2 mt-2 rounded-4xl hover:bg-blue-600 cursor-pointer w-30"
-        onClick={ () => setNewTask(true) }>
+          type="button"
+          className="bg-emerald-800 text-white p-2 mt-2 rounded-4xl hover:bg-blue-600 cursor-pointer w-30"
+          onClick={ () => setNewTask(true) }>
           Nova Tarefa
         </button>
       </div>
 
       {newTask &&
-      <form className="mx-auto w-fit flex flex-col">
+      <form
+        className="mx-auto w-fit flex flex-col"
+        onSubmit={handleSubmit}>
         <h1 className="text-2xl text-center">Tarefa</h1>
         
         <input
-        type="text"
-        placeholder="Insira o Título"
-        className="bg-gray-50 p-2 border rounded"
-        onChange={ (e) => setTitle(e.target.value) }/>
+          type="text"
+          placeholder="Insira o Título"
+          className="bg-gray-50 p-2 border rounded"
+          onChange={ (e) => setTitle(e.target.value) }/>
 
         <input
-        type="text"
-        placeholder="Insira a Descrição"
-        className="bg-gray-50 p-2 border rounded"
-        onChange={ (e) => setDescription(e.target.value) }/>
+          type="text"
+          placeholder="Insira a Descrição"
+          className="bg-gray-50 p-2 border rounded"
+          onChange={ (e) => setDescription(e.target.value) }/>
         
         <select
-        className="bg-gray-50 p-2 border rounded"
-        value={priority}
-        onChange={ (e) => setPriority(e.target.value) }>
-          <option disabled selected>Selecione a Prioridade</option>
+         className="bg-gray-50 p-2 border rounded"
+         value={priority}
+         onChange={ (e) => setPriority(e.target.value) }>
+          <option value="" disabled >Selecione a Prioridade</option>
           <option value="baixa">Baixa</option>
           <option value="media">Média</option>
           <option value="alta">Alta</option>
@@ -53,18 +85,27 @@ export default function Home() {
         </select>
         
         <select
-        className="bg-gray-50 p-2 border rounded"
-        value={status}
-        onChange={ (e) => setStatus(e.target.value) }>
-          <option disabled selected>Selecione a Categoria</option>
+          className="bg-gray-50 p-2 border rounded"
+          value={status}
+          onChange={ (e) => setStatus(e.target.value) }>
+
+          <option disabled>Selecione a Categoria</option>
           <option value="fazer">A Fazer</option>
           <option value="andamento">Em Andamento</option>
           <option value="concluido">Concluído</option>
+
         </select>
         <button
-        className="bg-emerald-800 text-white p-1 mt-2 rounded-4xl hover:bg-blue-600 cursor-pointer"
-        onClick={ () => setNewTask(false)}>
+          className="bg-red-900 text-white p-1 mt-2 rounded-4xl hover:bg-blue-600 cursor-pointer"
+          onClick={ () => setNewTask(false)}
+          type="button">
           Cancelar Tarefa
+        </button>
+        
+        <button
+          className="bg-emerald-800 text-white p-1 mt-2 rounded-4xl hover:bg-blue-600 cursor-pointer"
+          type="submit">
+          Concluir
         </button>
       </form>
       }
