a0683ce129e1362862f8bce2e502204d3558e596
Natã Junior <natajuniorpinheirodasilva@gmail.com>
2026-07-28 18:57:06 -0300

    add seed

---
+++ prisma/seed.ts
@@ -0,0 +1,44 @@
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3"
+import { PrismaClient } from "../src/generated/prisma/client"
+
+const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL ?? "file:./dev.db" })
+const prisma = new PrismaClient({ adapter })
+
+async function main() {
+    const existingBoard = await prisma.board.findFirst()
+    if (existingBoard) {return}
+
+    await prisma.board.create({
+        data: {
+            title: "Exemplo",
+            columns: {
+                create: [
+                    {title: "A Fazer", position: 100, cards: {
+                        create: [
+                            {title: "A Fazer 1", position: 100}
+                        ]
+                    } },
+                    {title: "Em Andamento", position: 200, cards: { 
+                        create: [
+                            {title: "Em Andamento 1", position: 200}
+                        ] 
+                    } },
+                    {title: "Concluído", position: 300, cards: {
+                        create: [
+                            {title: "Concluído 1", position: 300}
+                        ] 
+                    } },
+                ]
+            }
+        }
+    })
+}
+
+main()
+    .catch((e) => {
+        console.error(e)
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
