6023eda79a22369acaa42f505db4a4a7c1d2118b
Natã Junior <natajuniorpinheirodasilva@gmail.com>
2026-08-06 20:39:52 -0300

    Kanban stylization

--- src/components/Kanban.tsx
+++ src/components/Kanban.tsx
@@ -7,17 +7,25 @@ type Props = {
 function Kanban ({ board }: Props) {
 
 return(    
-    
-    <div>
+    <div className="flex justify-center" >
+        <div className="flex gap-5">
         {board.columns.map((column) => (
-            <div key={column.id}>
-                <h2>{column.title}</h2>
-            
+            <div className="flex flex-col w-72 gap-3 text" key={column.id}>
+            <h2 className="text-base font-semibold text-black/70 uppercase tracking-wide rounded border-b pb-1 shadow">
+                {column.title}
+            </h2>
+
             {column.cards.map((card) => (
-                <div key={card.id}>{card.title}</div>
+                <div
+                key={card.id}
+                className="bg-white/5 border border-black/20 rounded-xl p-4 text-black shadow"
+                >
+                {card.title}
+                </div>
             ))}
             </div>
         ))}
+        </div>
     </div>
 
 )}
