d71bd21ac8c0b11c5e0d2841de888c3707104607
Natã Junior <natajuniorpinheirodasilva@gmail.com>
2026-08-08 21:37:43 -0300

    organize the indentation

--- src/app/api/cards/route.ts
+++ src/app/api/cards/route.ts
@@ -3,8 +3,12 @@ import { prisma } from "@/lib/Prisma"
 export async function POST(request: Request) {
     const body = await request.json()
     const { title, columnId } = body
-    if (!title || title.length.trim() === 0) {return Response.json({ error: "Invalid title." }, { "status": 400 })}
-    if (!columnId || columnId.length === 0) {return Response.json({ error: "Invalid column." }, { "status": 400 })}
+    if (!title || title.length.trim() === 0) {
+        return Response.json({ error: "Invalid title." }, { "status": 400 })
+    }
+    if (!columnId || columnId.length === 0) {
+        return Response.json({ error: "Invalid column." }, { "status": 400 })
+    }
 
     const lastCard = await prisma.card.findFirst({
         where: { columnId },
