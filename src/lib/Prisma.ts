07e4e1da61d88431c8610819e60c4e730312b43d
Natã Junior <natajuniorpinheirodasilva@gmail.com>
2026-08-30 15:54:34 -0300

    fix: use injected environment variables on Vercel

--- src/lib/Prisma.ts
+++ src/lib/Prisma.ts
@@ -1,8 +1,6 @@
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3"
 import { PrismaClient } from "../../src/generated/prisma/client"
 
-process.loadEnvFile()
-
 const databaseUrl = process.env.DATABASE_URL
 if(!databaseUrl) {
   throw new Error("DATABASE_URL is not defined.")
